7c95db6ef8cee592619270ec0f4db7a8df980a46
JustinRM104 <justrom104@gmail.com>
2020-12-04 11:54:24 +0100

    filmpje 5

--- animaties.js
+++ animaties.js
@@ -2,3 +2,8 @@ document.querySelector('.animeer-knop').addEventListener('click', (e) => {
     document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts');
     document.querySelector('.vlakje2').classList.toggle('vlakje__naar-links');
 });
+
+setTimeout( () => {
+    document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts');
+    document.querySelector('.vlakje2').classList.toggle('vlakje__naar-links');    
+}, 2000);
