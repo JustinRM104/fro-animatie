656e782c570ab68d55bc9cfe93881332b4e9dcd9
JustinRM104 <justrom104@gmail.com>
2020-12-04 12:45:02 +0100

    filmpje 6

--- animaties.js
+++ animaties.js
@@ -7,3 +7,15 @@ setTimeout( () => {
     document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts');
     document.querySelector('.vlakje2').classList.toggle('vlakje__naar-links');    
 }, 2000);
+
+const menuKnop = document.querySelector('.nav__knop');
+const navItems = document.querySelectorAll('.nav__link');
+
+menuKnop.addEventListener('click', () => {
+    navItems.forEach((item, index) => {
+        setTimeout( () => {
+            item.classList.toggle('nav__link--shuif-in');
+
+        }, 100 * index);
+    });
+});
